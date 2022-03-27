60966a1e56c0ab0b08b555a344b529964543b2e5
pica2108 <gpicatoste@hotmail.com>
2022-03-27 19:58:41 +0200

    ejercicio 2

--- src/main.py
+++ src/main.py
@@ -1,17 +1,96 @@
 import json
 import sqlite3
 import pandas as pd
-from src.SQLite import connectBD, insertTable
+from src.SQLite import connectBD, insertTable, queryOne, queryAll
 from src.legal import parseLegalJson
 from src.users import parseUsersInfo, parseUsersDatesIps
 from src.utils import readJson
 
+# LECTURA DE LOS JSON Y CREACION DE TABLAS
+
 con = connectBD()
-#legal_table = parseLegalJson()
+legal_table = parseLegalJson()
 #insertTable(con, legal_table, "legal")
 
 users_table_info = parseUsersInfo()
 users_table_ips_dates = parseUsersDatesIps()
 
-insertTable(con, users_table_info, "users_info")
-insertTable(con, users_table_ips_dates, "users_ips_dates")
+#insertTable(con, users_table_info, "users_info")
+#insertTable(con, users_table_ips_dates, "users_ips_dates")
+
+print("EJERCICIO 2.MUESTRAS")
+
+telefonos = queryOne(con, "SELECT COUNT(telefono) FROM users_info WHERE telefono!='None'")
+contrasena = queryOne(con, "SELECT COUNT(contrasena) FROM users_info WHERE contrasena!='None'")
+provincia = queryOne(con, "SELECT COUNT(provincia) FROM users_info WHERE provincia!='None'")
+permisos = queryOne(con, "SELECT COUNT(permisos) FROM users_info WHERE permisos!='None'")
+emailsTotal = queryOne(con, "SELECT COUNT(emailsTotal) FROM users_info WHERE emailsTotal!='None'")
+emailsPhishing = queryOne(con, "SELECT COUNT(emailsPhishing) FROM users_info WHERE emailsPhishing!='None'")
+emailsCliclados = queryOne(con, "SELECT COUNT(emailsCliclados) FROM users_info WHERE emailsCliclados!='None'")
+fechas = queryOne(con, "SELECT COUNT(DISTINCT username) FROM users_ips_dates WHERE dates != 'None'")
+ips = queryOne(con, "SELECT COUNT(DISTINCT username) FROM users_ips_dates WHERE ips!='None'")
+
+aux = {
+    "telefonos": telefonos,
+    "provincia": provincia,
+    "contrasena": contrasena,
+    "permisos": permisos,
+    "emailTotal": emailsTotal,
+    "emailsPhishing": emailsPhishing,
+    "emailsCliclados": emailsCliclados,
+    'fechas': fechas,
+    'ips': ips
+}
+df_aux = pd.DataFrame(aux)
+print("EJERCICIO 2 ")
+print("NUMERO DE MUESTRAS (Por clave):")
+print(df_aux)
+print("NUMERO DE MUESTRAS (Por usuario completo):")
+non_missing_users = queryOne(con, "SELECT COUNT(*) FROM users_info WHERE isMissing=0")
+print(non_missing_users)
+
+print("EJERCICIO 2.MEDIA Y DS DE LAS FECHAS")
+
+numFechas = queryAll(con, "SELECT username, COUNT(dates) FROM users_ips_dates WHERE isMissing=0 GROUP BY username")
+df_numFechas = pd.DataFrame(numFechas, columns=['username', 'numDates'])
+print("Media: ")
+print(df_numFechas['numDates'].mean())
+print("Desviación Estandar:")
+print(df_numFechas['numDates'].std())
+
+print("EJERCICIO 2.MEDIA Y DS DE LAS IPS")
+
+numIps = queryAll(con, "SELECT username, COUNT(ips) FROM users_ips_dates WHERE isMissing=0 GROUP BY username")
+df_numIps = pd.DataFrame(numIps, columns=['username', 'numIps'])
+print("Media: ")
+print(df_numIps['numIps'].mean())
+print("Desvición Estandar: ")
+print(df_numIps['numIps'].std())
+
+print("EJERCICIO 2.MEDIA Y DS DE LOS EMAILS TOTALES")
+
+numEmailsTotal = queryAll(con, "SELECT emailsTotal FROM users_info WHERE isMissing=0")
+df_numEmailsTotal=pd.DataFrame(numEmailsTotal, columns=['numEmails'])
+print("Media: ")
+print(df_numEmailsTotal['numEmails'].mean())
+print("Desvición Estandar: ")
+print(df_numEmailsTotal['numEmails'].std())
+
+
+print("EJERCICIO 2.VALORES MAXIMOS Y MINIMOS DEL TOTAL DE FECHAS")
+
+numDates = queryAll(con, "SELECT username, COUNT(dates) FROM users_ips_dates WHERE isMissing=0 GROUP BY username")
+df_numDates = pd.DataFrame(numDates, columns=['username', 'dates'])
+print("Fechas mínimos")
+print(df_numDates['dates'].min())
+print("Fechas máximos")
+print(df_numDates['dates'].max())
+
+print("EJERCICIO 2.VALORES MAXIMOS Y MINIMOS DEL NUMERO DE EMAILS RECIBIDOS")
+
+emails_recibidos = queryAll(con, "SELECT emailsTotal FROM users_info WHERE isMissing=0")
+df_emails_recibidos = pd.DataFrame(emails_recibidos, columns=['emails'])
+print("Correos mínimos")
+print(df_emails_recibidos['emails'].min())
+print("Correos máximos")
+print(df_emails_recibidos['emails'].max())
--- src/users.py
+++ src/users.py
@@ -4,17 +4,25 @@ import pandas as pd
 from src.utils import readJson
 
 
+def isMissing(user):
+    for user_key in user.keys():
+        if user[user_key] == 'None' or user[user_key] == -1:
+            return 1
+    return 0
+
 def parseUsersInfo():
     data_users = readJson("./data/users.json")
     data_username_list = []
     for users_username in data_users['usuarios']:
         username = list(users_username.keys())[0]
         user_info = users_username[username]
+        user_info['isMissing'] = isMissing(user_info)
         del user_info["ips"]
         del user_info["fechas"]
         user_info['username'] = username
         data_username_list.append(user_info)
-    return pd.json_normalize(data_username_list)
+    df_users = pd.json_normalize(data_username_list)
+    return df_users.rename(columns={'emails.total': 'emailsTotal', 'emails.phishing': 'emailsPhishing', 'emails.cliclados': 'emailsCliclados'})
 
 
 
@@ -27,7 +35,8 @@ def parseUsersDatesIps():
         aux = {
             "ips": np.array(user_info["ips"]),
             "dates": np.array(user_info["fechas"]),
-            "username": username
+            "username": username,
+            "isMissing": isMissing(user_info)
         }
         df_aux = pd.DataFrame(aux)
         df_ips_dates = pd.concat([df_ips_dates, df_aux])
